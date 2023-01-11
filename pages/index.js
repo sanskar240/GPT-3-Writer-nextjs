dbd38c6c800d5ed6c870b83974b1ccdd6517e2f8
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2023-01-12 01:12:13 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { useState } from "react";
+
+
+const Home = () => {
+  const [userInput,setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  
+ 
+  const onChangedUserName  = (event) =>{
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  }
+
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Generate a Tweet</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Prompt the pointers,we'll generate the rest!</h2>
+          </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea 
+          value={userInput}
+          onChange = {onChangedUserName}
+          placeholder="start typing here" 
+          className="prompt-box" 
+          />
+           <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+           <div className="prompt-buttons">
+    <a className="generate-button" onClick={callGenerateEndpoint}>
+      <div className="generate">
+        <p>Generate</p>
+      </div>
+    </a>
+  </div>
+          <div>
+            
+          </div>
+        </div>
+        
+      </div>
+      <div className="badge-container grow">
+       
+          <div className="badge">
+          
+            <p>build with buildspace</p>
+          </div>
+
+        
+      </div>
+    </div>
+    
+  );
+};
+
+export default Home;
